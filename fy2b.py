d2260faeb03a45dbd8fe2352e1037e787d6d8ae8
shiweizhi <shiwzhi@gmail.com>
2017-01-20 15:45:24 +0800

    Modify

--- fy2b.py
+++ fy2b.py
@@ -5,13 +5,15 @@ from time import sleep
 import os
 import base64
 
-fy2b_dir = "/var/www/flask_y2b"
-vid_url = "https://swz1994.com/fy2b/videos"
+fy2b_dir = "/root/fy2b"
+vid_url = "https://swz1994.com/fy2b/videos/"
+
+player_html = "play.html"
 
 def download_video(input_vid):
 	if os.path.isfile(fy2b_dir+'/videos/{}.lock'.format(input_vid)) == False:
 		subprocess.call(["touch", fy2b_dir+"/videos/{}.lock".format(input_vid)])
-		subprocess.Popen(["youtube-dl", '-f', 'bestvideo[height<=480]+bestaudio[ext=m4a]', "www.youtube.com/watch?v={}".format(input_vid), '-o', fy2b_dir+'/videos/%(id)s.mp4'])
+		subprocess.Popen(["youtube-dl", '-f', 'bestvideo[height<=480]+bestaudio[ext=m4a]', "https://www.youtube.com/watch?v={}".format(input_vid), '-o', fy2b_dir+'/videos/%(id)s.mp4'])
 
 
 app = Flask(__name__)
@@ -40,20 +42,18 @@ def downloader(url="https://www.baidu.com"):
 def index():
 	if request.method == "GET":
 		download_video(request.args.get('v'))
-	return(render_template("temp.html", vid = request.args.get('v')))
+	return(render_template(player_html, vid = request.args.get('v')))
 
-@app.route("/video/")
 @app.route("/video")
 @app.route("/video/<v_id>")
 def video(v_id='404'):
-	status = requests.head("http://swz1994.com/videos/{}.mp4".format(v_id))
-	while str(status.status_code)[0] == '3':
-		status = requests.head(status.headers["Location"])
+	vid_file_url = vid_url+vid_url+".mp4"
+	status = requests.head(vid_file_url)
 	if str(status.status_code)[0] != '2':
-		sleep(1)
 		return(redirect(request.url))
+		# status = requests.head(status.headers["Location"])
 	else:
-		return(redirect("https://swz1994.com/videos/"+v_id+".mp4"))
+		return(redirect(request.url))
 
 
 if __name__ == "__main__":
